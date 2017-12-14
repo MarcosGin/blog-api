970bcf67f5c13061af9fa66b169dfdf17629bf1b
Marcos Gin <marcosgin291@gmail.com>
2017-12-14 17:40:39 -0300

    We use the grunt manager to perform typescript compilation tasks, scass, and copy the files.

---
+++ gruntfile.js
@@ -0,0 +1,76 @@
+module.exports = function (grunt) {
+    "use strict";
+
+    grunt.initConfig({
+        sass: {
+            dist: {
+                options: { noCache: true, style: 'compressed', sourcemap: 'none'},
+                files: [{
+                    expand: true,
+                    cwd: './src/public/assets/css',
+                    src: ['*.scss'],
+                    dest: './dist/public/assets/css',
+                    ext: '.css'
+                }]
+            }
+        },
+        copy: {
+            build: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: "./src/public",
+                        src: ["**",'!**/*.scss'],
+                        dest: "./dist/public"
+                    },
+                    {
+                        expand: true,
+                        cwd: "./views",
+                        src: ["**"],
+                        dest: "./dist/views"
+                    }
+                ]
+            }
+        },
+        ts: {
+            app: {
+                files: [{
+                    src: ["src/\*\*/\*.ts", "!src/.baseDir.ts"],
+                    dest: "./dist"
+                }],
+                options: {
+                    module: "commonjs",
+                    target: "es5",
+                    sourceMap: false,
+                    rootDir: "src"
+                }
+            }
+        },
+        watch: {
+            ts: {
+                files: ["src/\*\*/\*.ts"],
+                tasks: ["ts"]
+            },
+            sass: {
+                files: ["src/public/assets/css/\*.scss"],
+                tasks: ["sass"],
+            },
+            views: {
+                files: ["views/**/*.pug"],
+                tasks: ["copy"]
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks("grunt-contrib-copy");
+    grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadNpmTasks("grunt-ts");
+    grunt.registerTask("default", [
+        "sass",
+        "copy",
+        "ts",
+        "watch"
+    ]);
+
+};
