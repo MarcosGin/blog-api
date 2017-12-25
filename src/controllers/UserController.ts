bf7919e97679bbf46d811667331a2a1de511c26c
Marcos Gin <marcosgin291@gmail.com>
2017-12-25 20:12:20 -0300

    We updated the driver with the new methods of the User model

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,52 +1,64 @@
 import { Request, Response } from 'express';
 import * as _ from "lodash";
-import User from '../models/User';
+import { User, IUser } from '../models/models';
 
 export class UserController {
 
     public getAll(req: Request, res: Response): void {
-        User.find()
-            .then((data) => res.json({ users: data }))
-            .catch((err) => res.json({ error: err.errmsg }))
+        User.find(( err, data) => {
+            if(err){
+                return res.status(500).json({ error: err });
+            }
+            res.json({ users: data });
+        });
     }
 
     public get(req: Request, res: Response): void {
         const username: string = req.params.username;
-        User.findOne({ username }).populate('posts', 'title content')
-            .then((data) => {
-                if(data){ res.json(data); }else{ res.json( {message: 'The user not was exists'}); }
-            })
-            .catch((err) => res.json({ error: err.errmsg }))
+        User.findByUsername(username, (err, data) => {
+            if (err) {
+                return res.status(500).json({ error: err });
+            } 
+            if(data){ res.json(data); }else{ res.status(404).json({message: 'Not found this user'}); }
+        });
+            
     }
 
     public create(req: Request, res: Response): void {
-        const user = new User();
+        const user: IUser = new User();
         _.assign(user, req.body);
         
-        user.save()
-            .then((data) => res.json({ message: 'The user was created succesfully' }))
-            .catch((err) => res.json({ error: err.errmsg }))
+        user.save((err, data) => {
+            if (err) {
+                return res.status(500).json({ error: err });
+            }
+            res.json({ message: 'The user was created succesfully' });
+        });
     }
 
 
     public update(req: Request, res: Response): void {
         const username: string = req.params.username;
-        User.findOneAndUpdate({ username }, req.body)
-            .then((data) => res.json({ message: 'The user was updated succesfully' }))
-            .catch((err) => res.json({ error: err.errmsg }))
+        User.findOneAndUpdate({ username }, req.body, (err, data) => {
+            if (err) {
+                return res.status(500).json({ error: err });
+            }
+            res.json({ message: 'The user was updated succesfully' });
+        });
     }
 
     public delete(req: Request, res: Response): void {
         const username: string = req.params.username;
-        User.findOneAndRemove({ username })
-            .then((data) => { 
-                if(data){ 
-                    res.json({ message: 'The user was deleted succesfully' }); 
-                }else{ 
-                    res.json({ message: 'The user not was deleted succesfully' }); 
-                }
-            })
-            .catch((err) => res.json({ error: err.errmsg }))
+        User.findOneAndRemove({ username }, (err, data) => {
+            if(err){
+                return res.status(500).json({ error: err });
+            }
+            if (data) {
+                res.json({ message: 'The user was deleted succesfully' });
+            } else {
+                res.json({ message: 'The user not was deleted succesfully' });
+            }
+        });
     }
 
 }
